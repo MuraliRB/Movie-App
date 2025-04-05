629bc3c937466be20b77a4bad08caf7aaab73523
MuraliRB <muralirb1999@gmail.com>
2025-04-05 14:27:18 +0530

    feat: Enhance UI with trending movies display and improved layout components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,10 @@
 import MovieCard from "@/components/MovieCard";
 import SearchBox from "@/components/SearchBox";
+import TrendingCard from "@/components/TrendingCard";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
+import { getTrendingMovies } from "@/services/appwrite";
 import useFetch from "@/services/useFetch";
 import { Link, useRouter } from "expo-router";
 import {
@@ -18,6 +20,11 @@ import {
 export default function Index() {
   const router = useRouter();
   const {
+    data: trendingMovies,
+    loading: trendingLoading,
+    error: trendingError,
+  } = useFetch(getTrendingMovies);
+  const {
     data: movies,
     loading: moviesLoading,
     error: moviesError,
@@ -31,20 +38,37 @@ export default function Index() {
         contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        {moviesLoading ? (
+        {moviesLoading || trendingLoading ? (
           <ActivityIndicator
             size={"large"}
             color={"#0000ff"}
             className="mt-10 self-center"
           />
-        ) : moviesError ? (
-          <Text>Error: {moviesError?.messsage}</Text>
+        ) : moviesError || trendingError ? (
+          <Text>Error: {moviesError?.messsage || trendingError?.message}</Text>
         ) : (
           <View className="flex-1 mt-5">
             <SearchBox
               onPress={() => router.push("/search")}
               placeholder="Search for a movie"
             />
+            {trendingMovies && (
+              <View className="mt-10">
+                <Text className="text-lg text-white font-bold mt-5 mb-3">
+                  Trending Movies
+                </Text>
+                <FlatList
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  ItemSeparatorComponent={() => <View className="w-4" />}
+                  data={trendingMovies}
+                  renderItem={({ item, index }) => (
+                    <TrendingCard movie={item} index={index} key={index} />
+                  )}
+                  keyExtractor={(item) => item.movie_id.toString()}
+                />
+              </View>
+            )}
             <>
               <Text className="text-lg text-white font-bold mt-5 mb-3">
                 Latest movies
--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -4,10 +4,17 @@ import { icons } from "@/constants/icons";
 
 interface SearchBoxProps {
   placeholder: string;
-  onPress: () => void;
+  onPress?: () => void;
+  value?: string;
+  onChangeText?: (text: string) => void;
 }
 
-const SearchBox = ({ onPress, placeholder }: SearchBoxProps) => {
+const SearchBox = ({
+  onPress,
+  placeholder,
+  value,
+  onChangeText,
+}: SearchBoxProps) => {
   return (
     <View className="flex-row items-center bg-dark-200 rouded-full px-5 py-4">
       <Image
@@ -19,8 +26,8 @@ const SearchBox = ({ onPress, placeholder }: SearchBoxProps) => {
       <TextInput
         onPress={onPress}
         placeholder={placeholder}
-        value=""
-        onChangeText={() => {}}
+        value={value}
+        onChangeText={onChangeText}
         placeholderTextColor={"#a8b5db"}
         className="flex-1 ml-2 text-white"
       />
